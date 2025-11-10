7354f0adde6feb5ae2035d946604ae9b8aa1a1cb
Slay-Stina <august.hogbom@gmail.com>
2025-11-10 15:55:46 +0100

    BH-17 User Repository interface

---
+++ Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository.Interfaces;
+
+public interface IUserRepository
+{
+    public List<User> Get();
+    public User GetById();
+    public bool Create();
+    public bool Update();
+    public bool Delete();
+}
