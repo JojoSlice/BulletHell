d3ba83f8c902af7fe8b0c28e8f241dda38508203
Slay-Stina <august.hogbom@gmail.com>
2025-11-10 15:57:00 +0100

    BH-18 HighScore repository interface

---
+++ Repository/Interfaces/IHighScoreRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository.Interfaces;
+
+public interface IHighScoreRepository
+{
+    public List<HighScore> Get();
+    public HighScore GetById();
+    public bool Create();
+    public bool Update();
+    public bool Delete();
+}
