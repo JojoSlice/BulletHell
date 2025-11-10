3cad7af7d48880ff2e4027e8b889e838b1d252ff
Slay-Stina <august.hogbom@gmail.com>
2025-11-10 15:59:15 +0100

    BH-19 implementation of repository interfaces

---
+++ Repository/Repositories/HighScoreRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace Repository.Repositories;
+
+using Interfaces;
+
+public class HighScoreRepository : IHighScoreRepository
+{
+    public List<HighScore> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public HighScore GetById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Create()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Repository/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace Repository.Repositories;
+
+using Interfaces;
+
+public class UserRepository : IUserRepository
+{
+    public List<User> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User GetById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Create()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
