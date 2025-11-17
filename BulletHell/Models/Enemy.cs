eb43694d8a097712ff7d68327acc8784e2de2d2e
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-17 12:47:45 +0100

    BH-54 Added basic Enemy mdel skeleton (contructor, sprite, enemy Update, Draw, LoadContent)

---
+++ BulletHell/Models/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using BulletHell.Configurations;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BulletHell.Models;
+using BulletHell.Interfaces;
+using Microsoft.Xna.Framework;
+
+public class Enemy
+{
+    public Vector2 Position { get; private set; }
+    private Vector2 _velocity;
+    private readonly ISpriteHelper _sprite;
+
+    public int Width => _sprite.Width;
+    public int Height => _sprite.Height;
+
+    public bool IsAlive { get; private set; } =  true;
+
+    public Enemy(Vector2 startPosition, ISpriteHelper sprite)
+    {
+        Position = startPosition;
+        _sprite =  sprite;
+        _velocity = Vector2.Zero;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        
+    }
+
+    public void LoadContent(Texture2D texture)
+    {
+        
+    }
+}
