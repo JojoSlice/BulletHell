f55dd8cbad9f1ccb4976ce005029c1c5e0b7c8ce
Slay-Stina <august.hogbom@gmail.com>
2025-11-17 17:06:39 +0100

    BH-8 BH-63 GetAll & GetById

---
+++ Application/Interfaces/IHighScoreService.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Interfaces;
+
+using Contracts.Responses.HighScore;
+using Contracts.Responses.Shared;
+using Domain.Entities;
+
+public interface IHighScoreService
+{
+    public Task<Response<GetAllResponse<GetHighScoreResponse>>> GetAll();
+    public Task<Response<GetHighScoreResponse?>> GetById(int id);
+    public Response<CreateHighScoreResponse> Create(HighScore highScore);
+    public Response<UpdateHighScoreResponse> Update(int id, HighScore highScore);
+    public Response<string> Delete(int id);
+}
---
+++ Application/Services/HighScoreService.cs
@@ -0,0 +1,35 @@
+﻿namespace Application.Services;
+
+using Contracts.Responses.HighScore;
+using Contracts.Responses.Shared;
+using Domain.Entities;
+using Interfaces;
+using Mapping;
+using Repository.Repositories;
+
+public class HighScoreService(HighScoreRepository repository) : IHighScoreService
+{
+    public async Task<Response<GetAllResponse<GetHighScoreResponse>>> GetAll() =>
+        (await repository.GetAsync()).MapToResponse();
+
+    public async Task<Response<GetHighScoreResponse?>> GetById(int id) =>
+        (await repository.GetByIdAsync(id)).MapToResponse();
+
+    public Response<CreateHighScoreResponse> Create(HighScore highScore)
+    {
+        //TODO: Implement Create
+        throw new NotImplementedException();
+    }
+
+    public Response<UpdateHighScoreResponse> Update(int id, HighScore highScore)
+    {
+        //TODO: Implement Update
+        throw new NotImplementedException();
+    }
+
+    public Response<string> Delete(int id)
+    {
+        //TODO: Implement Delete
+        throw new NotImplementedException();
+    }
+}
