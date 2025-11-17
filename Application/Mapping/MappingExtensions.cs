6b54e97a2122822df4e47edf2ed2275f719555dc
Slay-Stina <august.hogbom@gmail.com>
2025-11-17 17:06:39 +0100

    BH-8 BH-63 GetAll & GetById

---
+++ Application/Mapping/MappingExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Application.Mapping;
+
+using Contracts.Responses.HighScore;
+using Contracts.Responses.Shared;
+using Domain.Entities;
+
+public static class MappingExtensions
+{
+    private static GetHighScoreResponse MapHighScoreToResponse(this HighScore hs) => new()
+    {
+        Id = hs.Id,
+        Score = hs.Score,
+        UserId = hs.UserId
+    };
+    
+    public static Response<GetAllResponse<GetHighScoreResponse>> MapToResponse(this List<HighScore> highScores)
+    {
+        return highScores.Count != 0 ? new Response<GetAllResponse<GetHighScoreResponse>>
+        {
+            IsSuccess = highScores.Count != 0,
+            Data = new GetAllResponse<GetHighScoreResponse>
+            {
+                Items = highScores.Select(hs => hs.MapHighScoreToResponse())
+            }
+        }
+        : new Response<GetAllResponse<GetHighScoreResponse>>
+        {
+            IsSuccess = false,
+            Data = null
+        };
+    }
+
+    public static Response<GetHighScoreResponse?> MapToResponse(this HighScore? highScore) => new()
+    {
+        IsSuccess = true,
+        Data = highScore?.MapHighScoreToResponse()
+    };
+}
