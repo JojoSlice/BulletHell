a5461af45f1907e7651adaa36bb2d03e5e2933dc
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-17 14:11:47 +0100

    BH-50 Add IEnemyManager interface

---
+++ BulletHell/Interfaces/IEnemyManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace BulletHell.Interfaces;
+
+public interface IEnemyManager
+{
+    void LoadContent(Texture2D enemyTexture);
+    void AddEnemy(Vector2 position); // här kan man lägga till pattern eller annat skoj sen
+    void Update(GameTime gameTime, int screenWidth, int screenHeight);
+    void Draw(SpriteBatch spriteBatch);
+}
