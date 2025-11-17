61ec8a95c3e915d539672e9c35d355c81cf6cfcd
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-17 14:16:00 +0100

    BH-50 Add EnemyManager skeleton implementing IEnemyManager

---
+++ BulletHell/Managers/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using BulletHell.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BulletHell.Managers;
+
+public class EnemyManager : IEnemyManager
+{
+    public void LoadContent(Texture2D enemyTexture)
+    {
+        
+    }
+
+    public void AddEnemy(Vector2 position)
+    {
+        
+    }
+
+    public void Update(GameTime gameTime, int screenWidth, int screenHeight)
+    {
+        
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        
+    }
+}
