90d49799f70699262f43287d45d004b68a3eb5d3
Slay-Stina <august.hogbom@gmail.com>
2025-11-17 17:36:09 +0100

    BH-8 BH-63 Create, Update & Delete Highscore + refactoring av responses

--- Application/Interfaces/IHighScoreService.cs
+++ Application/Interfaces/IHighScoreService.cs
@@ -6,9 +6,9 @@ using Domain.Entities;
 
 public interface IHighScoreService
 {
-    public Task<Response<GetAllResponse<GetHighScoreResponse>>> GetAll();
-    public Task<Response<GetHighScoreResponse?>> GetById(int id);
-    public Response<CreateHighScoreResponse> Create(HighScore highScore);
-    public Response<UpdateHighScoreResponse> Update(int id, HighScore highScore);
-    public Response<string> Delete(int id);
+    public Task<Response<List<HighScoreResponse>>> GetAll();
+    public Task<Response<HighScoreResponse?>> GetById(int id);
+    public Task<Response<HighScoreResponse>> Create(HighScore highScore);
+    public Task<Response<HighScoreResponse>> Update(HighScore highScore);
+    public Task<Response<string>> Delete(int id);
 }
