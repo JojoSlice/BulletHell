3174b2cdd50871ee2b5f461f3a2e52c73b3db4e5
JojoSlice <Johannes152@hotmail.com>
2025-11-18 11:32:17 +0100

    buttons implementastion VIP

--- BulletHell/Scenes/MenuScene.cs
+++ BulletHell/Scenes/MenuScene.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using BulletHell.Models;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -12,6 +14,44 @@ public class MenuScene(Game1 game) : Scene(game)
     private readonly string exitText = "Press ESC to quit";
     private readonly int screenWidth = game.GraphicsDevice.Viewport.Width;
     private readonly int screenHeight = game.GraphicsDevice.Viewport.Height;
+    private List<Button> menuButtons = [];
+
+    private Vector2 GetCenteredPosition(string text, float y)
+    {
+        Vector2 size = font.MeasureString(text);
+        return new Vector2(screenWidth / 2 - size.X / 2, y);
+    }
+
+    private List<string> GetMenuText() => [startText, exitText];
+
+    private List<Button> GetMenuButtons()
+    {
+        int padding = 0;
+        List<Button> buttons = [];
+        var menuText = GetMenuText();
+
+        foreach (var text in menuText)
+        {
+            var button = new Button(
+                _game.GraphicsDevice,
+                font,
+                text,
+                new(screenWidth / 2, screenHeight / 2 - padding, 200, 50)
+            );
+
+            padding += 60;
+
+            buttons.Add(button);
+        }
+
+        return buttons;
+    }
+
+    public override void OnEnter()
+    {
+        System.Console.WriteLine("MenuScene loading");
+        menuButtons = GetMenuButtons();
+    }
 
     public override void Update(GameTime gameTime)
     {
@@ -26,31 +66,12 @@ public class MenuScene(Game1 game) : Scene(game)
         }
     }
 
-    private Vector2 GetCenteredPosition(string text, float y)
-    {
-        Vector2 size = font.MeasureString(text);
-        return new Vector2(screenWidth / 2 - size.X / 2, y);
-    }
-
     public override void Draw(SpriteBatch spriteBatch)
     {
         spriteBatch.DrawString(font, title, GetCenteredPosition(title, 200), Color.Black);
-        spriteBatch.DrawString(
-            font,
-            startText,
-            GetCenteredPosition(startText, screenHeight / 2),
-            Color.Yellow
-        );
-        spriteBatch.DrawString(
-            font,
-            exitText,
-            GetCenteredPosition(exitText, screenHeight / 2 + 30),
-            Color.Gray
-        );
-    }
-
-    public override void OnEnter()
-    {
-        System.Console.WriteLine("MenuScene loading");
+        foreach (var button in menuButtons)
+        {
+            button.Draw(spriteBatch);
+        }
     }
 }
