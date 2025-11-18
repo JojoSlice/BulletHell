520a8b066f4c9047eb2ad176b49e23ec1f83c13f
JojoSlice <Johannes152@hotmail.com>
2025-11-18 11:31:29 +0100

    menu buttons

---
+++ BulletHell/Models/Button.cs
@@ -0,0 +1,85 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BulletHell.Models;
+
+public class Button
+{
+    private readonly Rectangle _bounds;
+    private readonly string _text;
+    private readonly SpriteFont _font;
+    private readonly Texture2D _texture;
+
+    private bool _isHovered;
+    private bool _wasPressed;
+
+    public event Action? OnClick;
+
+    public Button(GraphicsDevice graphicsDevice, SpriteFont font, string text, Rectangle bounds)
+    {
+        _font = font;
+        _text = text;
+        _bounds = bounds;
+
+        _texture = new Texture2D(graphicsDevice, 1, 1);
+        _texture.SetData(new[] { Color.White });
+    }
+
+    public void Update(MouseState mouseState)
+    {
+        _isHovered = _bounds.Contains(mouseState.Position);
+
+        bool isPressed = mouseState.LeftButton == ButtonState.Pressed;
+
+        if (_wasPressed && !isPressed && _isHovered)
+        {
+            OnClick?.Invoke();
+        }
+
+        _wasPressed = isPressed && _isHovered;
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        // Bakgrundsfärg baserat på state
+        Color bgColor = _isHovered ? Color.Gray : Color.DarkGray;
+        if (_wasPressed)
+            bgColor = Color.DimGray;
+
+        // Rita bakgrund
+        spriteBatch.Draw(_texture, _bounds, bgColor);
+
+        // Rita ram
+        DrawBorder(spriteBatch, _bounds, Color.White, 2);
+
+        // Centrera text
+        Vector2 textSize = _font.MeasureString(_text);
+        Vector2 textPos = new(
+            _bounds.X + _bounds.Width / 2 - textSize.X / 2,
+            _bounds.Y + _bounds.Height / 2 - textSize.Y / 2
+        );
+        spriteBatch.DrawString(_font, _text, textPos, Color.White);
+    }
+
+    private void DrawBorder(SpriteBatch spriteBatch, Rectangle rect, Color color, int thickness)
+    {
+        // Topp
+        spriteBatch.Draw(_texture, new Rectangle(rect.X, rect.Y, rect.Width, thickness), color);
+        // Botten
+        spriteBatch.Draw(
+            _texture,
+            new Rectangle(rect.X, rect.Bottom - thickness, rect.Width, thickness),
+            color
+        );
+        // Vänster
+        spriteBatch.Draw(_texture, new Rectangle(rect.X, rect.Y, thickness, rect.Height), color);
+        // Höger
+        spriteBatch.Draw(
+            _texture,
+            new Rectangle(rect.Right - thickness, rect.Y, thickness, rect.Height),
+            color
+        );
+    }
+}
