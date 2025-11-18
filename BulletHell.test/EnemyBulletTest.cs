b6ec1baae77aa7b5038a1e853414a7f34094f566
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-18 21:53:53 +0100

    BH-56 Add initial EnemyBullet movement test and minimal implementation

---
+++ BulletHell.test/EnemyBulletTest.cs
@@ -0,0 +1,35 @@
+﻿using BulletHell.Helpers;
+using BulletHell.Managers;
+using BulletHell.Models;
+using Microsoft.Xna.Framework;
+using NSubstitute.Routing.Handlers;
+
+namespace BulletHell.test;
+
+public class EnemyBulletTest(ITestOutputHelper output)
+{
+    [Fact]
+    public void EnemyBullet_ShouldMoveAccordingToVelocity()
+    {
+        // Arrange
+        var startPosition = new Vector2(10, 10);
+        var velocity = new Vector2(10, 10);
+        var enemyBullet = new EnemyBullet(startPosition, velocity);
+
+        var deltaTime = 1/60f;
+        var gameTime = new GameTime(
+            TimeSpan.Zero,
+            TimeSpan.FromSeconds(deltaTime)
+        );
+
+        var expectedPosition = startPosition + velocity * deltaTime;
+
+        // Act
+        enemyBullet.Update(gameTime);
+        var actualPosition = enemyBullet.Position;
+        
+        // Assert
+        Assert.Equal(expectedPosition.X, actualPosition.X, 4);
+        Assert.Equal(expectedPosition.Y, actualPosition.Y, 4);
+    }
+}
