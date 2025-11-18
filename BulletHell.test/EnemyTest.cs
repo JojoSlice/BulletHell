84899ccf80f68babc06e833b9b6fc06f804247b4
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-18 15:29:14 +0100

    deleted test. the behavior is verified in another test

--- BulletHell.test/EnemyTest.cs
+++ BulletHell.test/EnemyTest.cs
@@ -44,27 +44,6 @@ public class EnemyTest
     }
 
     [Fact]
-    public void Update_ShouldCallSpriteUpdate()
-    {
-        // Arrange
-        var startPosition = new Vector2(10, 10);
-        var spriteMock = Substitute.For<ISpriteHelper>();
-        var enemy = new Enemy(startPosition, spriteMock);
-        var deltaTime = 1/60f;
-        var totalTime = TimeSpan.Zero;
-        var elapsedTime = TimeSpan.FromSeconds(deltaTime);
-        var gameTime = new GameTime(totalTime, elapsedTime);
-        
-        // Act
-        enemy.Update(gameTime);
-        
-        // Assert
-        spriteMock.Received(1).Update(gameTime);
-        
-        _output.WriteLine("sprite.Update(gameTime) was called once ✔");
-    }
-
-    [Fact]
     public void Update_ShouldUseEnemySpeed()
     {
         // Arrange
