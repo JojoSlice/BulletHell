49c79e37144d1cadf2a8e19b76be8f7bc4c4d826
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-21 21:57:03 +0100

    commit of the day- API start

---
+++ Api.test/BasicApiTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Api.Tests
+{
+    public class BasicApiTests
+    {
+        [Fact]
+        public async Task Api_Starts_WithoutErrors()
+        {
+            // Arrange
+            await using var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
--- Api/Endpoints/HighScoreEndpoints.cs
+++ Api/Endpoints/HighScoreEndpoints.cs
@@ -1,4 +1,4 @@
-﻿using Application.Services;
+﻿using Application.Interfaces;
 using Contracts;
 using Contracts.Requests.HighScore;
 using Contracts.Responses.HighScore;
@@ -10,23 +10,28 @@ public static class HighScoreEndpoints
     public static void MapHighScoreEndpoints(this WebApplication app)
     {
         // GET all HighScores
-        app.MapGet(ApiEndpoints.HighScore.GetAll, async ([FromServices] HighScoreService hss) =>
+        app.MapGet(ApiEndpoints.HighScore.GetAll, async ([FromServices] IHighScoreService<HighScoreResponse> hss
+            ) =>
         await hss.GetAll()).WithName("GetAllHighScores").Produces<List<HighScoreResponse>>();
 
         // GET HighScore by ID
-        app.MapGet(ApiEndpoints.HighScore.GetById, async (int id, [FromServices] HighScoreService hss) =>
+        app.MapGet(ApiEndpoints.HighScore.GetById, async (int id, [FromServices] IHighScoreService<HighScoreResponse> hss
+) =>
         await hss.GetById(id)).WithName("GetHighScoreById").Produces<HighScoreResponse?>();
 
         // POST HighScore
-        app.MapPost(ApiEndpoints.HighScore.Create, async (CreateHighScoreRequest createRequest, [FromServices] HighScoreService hss) =>
+        app.MapPost(ApiEndpoints.HighScore.Create, async (CreateHighScoreRequest createRequest, [FromServices] IHighScoreService<HighScoreResponse> hss
+            ) =>
         await hss.Create(createRequest)).WithName("CreateHighScore").Produces<HighScoreResponse>();
 
         // PUT HighScore
-        app.MapPut(ApiEndpoints.HighScore.Update, async (UpdateHighScoreRequest updateRequest, [FromServices] HighScoreService hss) =>
+        app.MapPut(ApiEndpoints.HighScore.Update, async (UpdateHighScoreRequest updateRequest, [FromServices] IHighScoreService<HighScoreResponse> hss
+            ) =>
         await hss.Update(updateRequest)).WithName("UpdateHighScore").Produces<HighScoreResponse>();
 
         // DELETE HighScore
-        app.MapDelete(ApiEndpoints.HighScore.Delete, async (int id, [FromServices] HighScoreService hss) =>
+        app.MapDelete(ApiEndpoints.HighScore.Delete, async (int id, [FromServices] IHighScoreService<HighScoreResponse> hss
+            ) =>
         await hss.Delete(id)).WithName("DeleteHighScore").Produces<string>();
     }
 }
