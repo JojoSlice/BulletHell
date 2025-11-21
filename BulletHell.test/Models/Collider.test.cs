7ae89637b3ec383f311bd306cca68d0389272167
Slay-Stina <august.hogbom@gmail.com>
2025-11-21 12:45:38 +0100

    BH-80 Collider model + object distance test

--- BulletHell.test/Models/Collider.test.cs
+++ BulletHell.test/Models/Collider.test.cs
@@ -1,12 +1,26 @@
-﻿using BulletHell.Models;
+﻿using System.Numerics;
+using BulletHell.Models;
 
 namespace BulletHell.test.Models;
 
-public class Collider_test
+public class ColliderTest
 {
-    [Fact]
-    public void Collider_ShouldReturnTrue()
+    [Theory]
+    [InlineData(2,2)]
+    [InlineData(4,7)]
+    [InlineData(6,12)]
+    [InlineData(8,17)]
+    [InlineData(10,22)]
+    [InlineData(12,27)]
+    public void Collider_CheckObjectsDistance_ReturnsCorrect(int x, int y)
     {
-        var collider = new Collider();
+        var object1 = new Vector2(1, 1);
+        var object2 = new Vector2(x, y);
+        var expectedDistance = ((object1.X - object2.X) * (object1.X - object1.X)) + ((object1.Y - object2.Y) * (object1.Y - object2.Y));
+        var collider = new Collider(object1, object2);
+
+        var actualDistance = collider.CheckDistance();
+
+        Assert.Equal(expectedDistance, actualDistance);
     }
 }
--- BulletHell/Models/Collider.cs
+++ BulletHell/Models/Collider.cs
@@ -1,6 +1,14 @@
-﻿namespace BulletHell.Models;
+﻿using System.Numerics;
 
-public class Collider
+namespace BulletHell.Models;
+
+public class Collider(Vector2 pos1, Vector2 pos2)
 {
-    
+    public Vector2 Position1 { get; init; } = pos1;
+    public Vector2 Position2 { get; init; } = pos2;
+
+    public float CheckDistance()
+    {
+        throw new System.NotImplementedException();
+    }
 }
