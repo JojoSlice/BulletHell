910ba42aa4a7bd6cc3f0a812e5b3a8b2911b4124
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-11-25 10:02:58 +0100

    added DebugUI

---
+++ BulletHell/DebugUI/DebugOverlay.cs
@@ -0,0 +1,26 @@
+﻿using BulletHell.Scenes;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BulletHell.Debug;
+
+public class DebugOverlay
+{
+    public bool IsVisible { get; private set; } = true;
+
+    private readonly SpriteFont _font;
+    private readonly Texture2D _bgTexture;
+
+    public DebugOverlay(SpriteFont font, Texture2D bgTexture)
+    {
+        _font = font;
+        _bgTexture = bgTexture;
+    }
+
+    public void Toggle() => IsVisible = !IsVisible;
+
+    public void Draw(SpriteBatch spriteBatch, BattleScene scene)
+    {
+        if (!IsVisible) return;
+
+    }
+}
