bbd2628b2c57b00206d93ad550fee4228a91dba2
Slay-Stina <august.hogbom@gmail.com>
2025-11-25 11:43:13 +0100

    Collision manager

--- BulletHell/Managers/CollisionManager.cs
+++ BulletHell/Managers/CollisionManager.cs
@@ -1,15 +1,36 @@
 ﻿using BulletHell.Models;
+using Microsoft.Xna.Framework;
 
 namespace BulletHell.Managers;
 
-public class CollisionManager(Collider collider1, Collider collider2)
+public class CollisionManager(Collider colliderA, Collider colliderB)
 {
     public bool IsColliding()
     {
-        throw new System.NotImplementedException();
+        if (colliderA.Equals(colliderB)
+            || (colliderA.ColliderType == colliderB.ColliderType
+                && colliderA.ColliderType != null)
+            || (colliderA.ColliderType == typeof(Player) && colliderB.ColliderType == typeof(Bullet))
+            || (colliderA.ColliderType == typeof(Enemy) && colliderB.ColliderType == typeof(EnemyBullet))
+            || (colliderA.ColliderType == typeof(Bullet) && colliderB.ColliderType == typeof(EnemyBullet)))
+        {
+            return false;
+        }
+
+        return RadiiSquared(colliderA, colliderB) >=
+               Vector2.DistanceSquared(colliderA.Position, colliderB.Position);
     }
+
     public float Distance()
     {
-        throw new System.NotImplementedException();
+        if (colliderA.Equals(colliderB))
+        {
+            return 0;
+        }
+
+        return Vector2.Distance(colliderA.Position, colliderB.Position);
     }
+
+    private float RadiiSquared(Collider collider1, Collider collider2) =>
+        (collider1.Radius + collider2.Radius) * (collider1.Radius + collider2.Radius);
 }
