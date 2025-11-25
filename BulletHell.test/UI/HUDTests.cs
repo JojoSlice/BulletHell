a4371998e3b5c2f51a79f4da8b839cb94b637095
Patrik Hildenborg Mac <phildenborg@gmail.com>
2025-11-25 11:33:45 +0100

    BH-99: Add auto-update of HUD Message when HP changes

--- BulletHell.test/UI/HUDTests.cs
+++ BulletHell.test/UI/HUDTests.cs
@@ -1,9 +1,17 @@
 using BulletHell.UI.Components;
+using Microsoft.Testing.Platform.Extensions.OutputDevice;
 
 namespace BulletHell.test.UI;
 
 public class HUDTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public HUDTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     [Fact]
     public void HUD_ShouldStoreAndReturnMessage()
     {
@@ -17,5 +25,24 @@ public class HUDTests
 
         // Assert
         Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void HUD_ShouldUpdateMessage_WhenHPChanges()
+    {
+        // Arrange
+        var expected = "HP: 50";
+        var hud = new HUD();
+
+        // Act
+        hud.HP = 50;
+        var actual = hud.Message;
+
+        // Assert
+        Assert.Equal(expected, actual);
+
+        // Output
+        _output.WriteLine($"Expected: {expected}");
+        _output.WriteLine($"Actual:   {actual}");
     }
 }
--- BulletHell/UI/Components/HUD.cs
+++ BulletHell/UI/Components/HUD.cs
@@ -3,4 +3,14 @@ namespace BulletHell.UI.Components;
 public class HUD
 {
     public string Message { get; set; }
+    private int _hp;
+    public int HP
+    {
+        get => _hp;
+        set
+        {
+            _hp = value;
+            Message = $"HP: {value}";
+        }
+    }
 }
