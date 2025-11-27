d4bd787d6fa152cbbbafca832863a9f4c1fdfe6a
Slay-Stina <august.hogbom@gmail.com>
2025-11-27 13:42:26 +0100

    BH-86 Add `Collider` to `EnemyBullet` model and ensure synchronization during updates with unit tests

--- BulletHell/Models/EnemyBullet.cs
+++ BulletHell/Models/EnemyBullet.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using BulletHell.Configurations;
-using BulletHell.Helpers;
 using BulletHell.Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,16 +11,20 @@ public class EnemyBullet : IDisposable
     private readonly ISpriteHelper _sprite;
     private float _timeAlive = 0f;
     private Vector2 _velocity;
+    private Collider _collider;
 
     public Vector2 Position { get; private set; }
     public int Width => _sprite.Width;
     public int Height => _sprite.Height;
+
+    public Collider Collider => _collider;
 
     public EnemyBullet(Vector2 startPosition, Vector2 velocity, ISpriteHelper sprite)
     {
         Position = startPosition;
         _velocity = velocity;
         _sprite = sprite;
+        _collider = new Collider(Position, typeof(EnemyBullet));
     }
 
     public bool ShouldBeRemoved(int screenWidth, int screenHeight)
@@ -36,13 +39,25 @@ public class EnemyBullet : IDisposable
             EnemyBulletConfig.SpriteWidth,
             EnemyBulletConfig.SpriteHeight,
             EnemyBulletConfig.AnimationSpeed);
+
+        // Set collider radius based on sprite size
+        UpdateColliderRadiusFromSprite();
     }
+
+    /// <summary>
+    /// Updates the collider radius from the current sprite frame size.
+    /// Exposed to allow unit tests to set radius without requiring a real Texture2D.
+    /// </summary>
+    public void UpdateColliderRadiusFromSprite() => _collider.Radius = Math.Max(Width, Height) / 2f;
 
     public void Update(GameTime gameTime)
     {
         var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
         Position += _velocity * deltaTime;
         _timeAlive += deltaTime;
+
+        // Keep collider position in sync
+        _collider.Position = Position;
 
         _sprite.Update(gameTime);
     }
@@ -65,6 +80,7 @@ public class EnemyBullet : IDisposable
         Position = position;
         _velocity = velocity;
         _timeAlive = 0f;
+        _collider.Position = Position;
     }
 
     public void Dispose()
