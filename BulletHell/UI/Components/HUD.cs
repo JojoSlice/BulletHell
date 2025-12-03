7cc76ef300af7194552d46f1763f888d16e612db
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-12-03 14:12:07 +0100

    add life icon rendering and sync HUD lives with Player

--- BulletHell/UI/Components/HUD.cs
+++ BulletHell/UI/Components/HUD.cs
@@ -11,6 +11,8 @@ public class HUD
     public int MaxHP { get; set; } = 100;
 
     public int Lives { get; set; } = PlayerConfig.Lives;
+    public Texture2D? LifeTexture { get; set; }
+
 
     public void UpdateLives(int lives)
     {
@@ -25,5 +27,18 @@ public class HUD
         // HP bar fylld
         int hpWidth = (int)(200 * (HP / (float)MaxHP));
         spriteBatch.Draw(TextureHelper.WhitePixel(spriteBatch.GraphicsDevice), new Rectangle(10, 10, hpWidth, 20), Color.LimeGreen);
+
+        // Draw lives below HP bar
+        if (LifeTexture != null)
+        {
+            for (int i = 0; i < Lives; i++)
+            {
+                spriteBatch.Draw(
+                    LifeTexture,
+                    new Vector2(10 + i * (LifeTexture.Width + 5), 40),
+                    Color.White
+                );
+            }
+        }
     }
 }
