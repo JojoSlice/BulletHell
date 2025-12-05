deb63b60ae6ada032e6814f6d58d1a9bee7fea87
Patrik Hildenborg Pc <phildenborg@gmail.com>
2025-12-05 09:55:17 +0100

    dd IHealth and ICollidable interfaces to improve decoupling and cohesion

---
+++ BulletHell/Interfaces/ICollidable.cs
@@ -0,0 +1,8 @@
+﻿using BulletHell.Models;
+
+namespace BulletHell.Interfaces;
+
+public interface ICollidable
+{
+    Collider Collider { get; }
+}
---
+++ BulletHell/Interfaces/IHealth.cs
@@ -0,0 +1,8 @@
+﻿namespace BulletHell.Interfaces;
+
+public interface IHealth
+{
+    int Health { get; }
+    void TakeDamage(int amount);
+    bool IsAlive { get; }
+}
